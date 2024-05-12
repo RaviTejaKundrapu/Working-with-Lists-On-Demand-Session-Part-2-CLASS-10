e03c5c6652ea61f0e1bd0dd30830bdf7bbc054b2
RaviTejaKundrapu <ravi.kundrapu123@gmail.com>
2024-05-12 09:11:45 +0000

    Working-with-Lists-On-Demand-Session-Part-2-CLASS-10

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, addFav} = props
+  const {id, name, mobileNo, isFavorite} = contactDetails
+  const toggleFav = () => {
+    addFav(id)
+  }
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={toggleFav}
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
